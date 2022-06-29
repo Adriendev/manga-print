a3ea362f21f4360c2e7eb30f9591ac915f3707fe
begoczb <begocz.31@gmail.com>
2022-06-29 17:46:07 +0200

    add carrousel

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import { useState } from "react";
+import SeriesCard from "./SeriesCard";
 
-const Carrousel = ({ title, card }) => {
+import "./Carrousel.css";
+
+const Carrousel = ({ title, series, slice }) => {
+  //   const [seriesId, setSeriesId] = useState([]);
+  const slicedSeries = series.slice(0, 10);
+
+  const listAllSeries = slicedSeries.map((elem) => (
+    <SeriesCard name={elem.name}></SeriesCard>
+  ));
+
+  listAllSeries.sort();
+
   return (
-    <section className={title}>
+    <section className="carrousel" key={title}>
       <h2>{title}</h2>
       <hr className="divider" />
+      <div>{listAllSeries}</div>
     </section>
   );
 };
---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+const API_URL = "https://manga-release-api.herokuapp.com/api";
+
+export { API_URL };
