e4f6c187460aa5c7e6ac25cbf8f2977b7f8f1ef7
begoczb <begocz.31@gmail.com>
2022-07-06 21:23:50 +0200

    implemented moda volume + css

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -7,7 +7,7 @@ import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { faStar as faStarRegular } from "@fortawesome/free-regular-svg-icons";
-
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
 
@@ -17,6 +17,8 @@ const ArrowRight = () => <FontAwesomeIcon icon={faAngleRight} />;
 const ArrowLeft = () => <FontAwesomeIcon icon={faAngleLeft} />;
 const FullStar = () => <FontAwesomeIcon icon={faStar} />;
 const EmptyStar = () => <FontAwesomeIcon icon={faStarRegular} />;
+
+const Cross = () => <FontAwesomeIcon icon={faXmark} />;
 
 const TrashCan = () => <FontAwesomeIcon icon={faTrashCan} />;
 const PenToSquare = () => <FontAwesomeIcon icon={faPenToSquare} />;
@@ -30,4 +32,5 @@ export {
   EmptyStar,
   TrashCan,
   PenToSquare,
+  Cross,
 };
--- src/pages/SeriesDetailsPage.jsx
+++ src/pages/SeriesDetailsPage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Modal from "react-modal";
 
 import "./SeriesDetailsPage.css";
 import { API_URL } from "../utils/constants";
@@ -8,13 +9,48 @@ import { API_URL } from "../utils/constants";
 import LoadingDisplay from "../components/LoadingDisplay";
 import SeriesDetails from "../components/SeriesDetails";
 import SeriesReviewsContainer from "../components/SeriesReviewsContainer";
+import VolumesContainer from "../components/VolumesContainer";
+import VolumeCard from "../components/VolumeCard";
+import { Cross } from "../components/Icon";
+import { faBlackboard } from "@fortawesome/free-solid-svg-icons";
+import { color } from "@mui/system";
+
+Modal.setAppElement("#root");
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    // backgroundColor: color("black"),
+  },
+};
 
 const SeriesDetailsPage = () => {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
   const seriesId = useParams().id;
   const [isLoading, setIsLoading] = useState(true);
   const [oneSeries, setOneSeries] = useState({});
   const [seriesVolumes, setSeriesVolumes] = useState([]);
   const [seriesRating, setSeriesRating] = useState(null);
+  const [volumeInModal, setVolumeInModal] = useState({});
+
+  function openModal(e) {
+    // console.log(e.currentTarget);
+    // console.log(`opened modal`);
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   useEffect(() => {
     setIsLoading(true);
@@ -52,6 +88,33 @@ const SeriesDetailsPage = () => {
         <SeriesReviewsContainer
           seriesId={seriesId}
           setSeriesRating={setSeriesRating}
+        />
+      </section>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Volume"
+      >
+        <div className="modal">
+          <button className="btn-close" onClick={closeModal}>
+            <Cross />
+          </button>
+          <VolumeCard
+            title={volumeInModal.title}
+            image={volumeInModal.image}
+            isbn={volumeInModal.isbn}
+            date={volumeInModal.date}
+            modal="true"
+          />
+        </div>
+      </Modal>
+      <section className="volumes-container">
+        <VolumesContainer
+          openModal={openModal}
+          volumes={seriesVolumes}
+          setVolumeInModal={setVolumeInModal}
         />
       </section>
     </main>
